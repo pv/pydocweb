f426c6752e21744c2e26fff12a402df82ba8d8b1
Emmanuelle Gouillart <gouillar@gimel>
2008-04-15 00:26:27 +0200

    cleaned the code to write on the wiki; started some hacks to write from the wiki

--- numpydoc.py
+++ numpydoc.py
@@ -6,6 +6,8 @@ This programs allows you to generate the numpy documentation wiki
 import inspect
 import numpy
 import editmoin
+import os
+import string
 
 class MoinPage():
 
@@ -23,46 +25,86 @@ class MoinPage():
         mf.read_raw(sourcefile)
         editmoin.sendfile(urlopener, self.url, mf)
 
+    def retrieve_docstring(self):
+        template = None
+        urlopener = editmoin.get_urlopener(self.url, id)
+        moinfile = editmoin.fetchfile(urlopener, self.url, id, template)
+        geturl = self.url+"?action=edit"
+        filename, headers = urlopener.retrieve(geturl)
+        mf = editmoin.MoinFile(filename, id)
+        dumpfile = "coucou"
+        filename = moinfile.write_file(dumpfile)
+        fid = open(dumpfile,'r')
+        lines = fid.readlines()
+        return lines[5:-1] #Needs better parsing of course !!
+        
+        
 
-def main(url):
-    """
-        Given a url address, writes a list of numpy functions to the url webpage, 
-        and creates an individual documentation page for each function.  
-    """
-    numpy_names = [name for name in inspect.getmembers(numpy) if (callable(name[1]) and \
-    (inspect.getdoc(name[1]) is not None)and inspect.isfunction(name[1]))]
-    mfp = MoinPage(url)
+class DocModificator():
 
-    # Open temporary file for storing information. This is a hack to reuse code in editmoin
-    file = open('numpy_list','w')
+    def __init__(self, module, wiki_url = None, base_dir = None):
+        self.module = module
+        self.wiki_url = wiki_url
+        self. base_dir = base_dir
 
-    # Generate front page, which is a numbered list of functions (1. with moin syntax)
-    for n in numpy_names:
-        file.write(' 1. [:/%s:%s]\n' % (n[0],n[0]))
-    file.close()
-    mfp.write_file('numpy_list')
+    def get_docs(self):
+        self.module_names = [name for name in inspect.getmembers(self.module) if \
+        (callable(name[1]) and (inspect.getdoc(name[1]) is not None) \
+        and inspect.isfunction(name[1]))]
 
-    # Generate a page for each function
-    for n,fn in numpy_names:
-        n_url = url +'/'+ n
-        print fn
-        mf = MoinPage(n_url)
-        # Another temporary file
-        file = open('temp','w')
-        n_doc = inspect.getdoc(fn) # doc
-        argspec = inspect.getargspec(fn) # arguments
-        args = inspect.formatargspec(*argspec)
-        file.write("== %s ==\n" %n) # title
-        file.write("{{{\n#!rst\n") # start code block in rst
-        file.write('**%s** ' %(n)+args+'\n')
-        file.write(inspect.getdoc(fn))
-        file.write("}}}\n") # end code block
+    def write_doc_to_wiki(self):
+        mfp = MoinPage(self.wiki_url)
+        # Open temporary file for storing information. 
+        # This is a hack to reuse code in editmoin 
+        file = open('numpy_list','w')
+        # Generate front page, which is a numbered list of functions (1. with moin syntax)
+        for n in self.module_names:
+            file.write(' 1. [:/%s:%s]\n' % (n[0],n[0]))
         file.close()
-        mf.write_file('temp') # write to page
+        mfp.write_file('numpy_list')
+
+        # Generate a page for each function
+        for n,fn in self.module_names:
+            n_url = os.path.join(self.wiki_url, n)
+            print n_url
+            print fn
+            fn_doc = function_doc(n,fn)
+            mf = MoinPage(n_url)
+            # Another temporary file
+            file = open('temp','w')
+            file.write(fn_doc)
+            file.close()
+            mf.write_file('temp') # write to page
+
+    def write_doc_from_wiki(self):
+        fl = filelist(base_dir)
 
 
-if __name__ == "__main__":
-    main("http://localhost:8000/NumpyDoc")
+    def write_fndoc_from_string(self,fn,string,fl):
+        fn_doc = inspect.getdoc(fn)
+        print fn_doc
+        for file in fl:
+            fid = open(file,'r')
+            st = fid.read()
+            if fn_doc in st:
+                print "trouve", file
+
+
+def function_doc(n,fn):
+    n_doc = inspect.getdoc(fn) # doc
+    argspec = inspect.getargspec(fn) # arguments
+    args = inspect.formatargspec(*argspec)
+    doc_str = "== %s ==\n"%n + "{{{\n#!rst\n"+'**%s** '%(n)+args+'\n\n'+ \
+        inspect.getdoc(fn) + "\n}}}\n"
+    return doc_str
+
+def filelist(base_dir):
+    fl = []
+    for (root, subs, files) in os.walk(base_dir):
+        fl += [os.path.join(root,file) for file in files]
+    return fl
+
+
 
 
 # vim:et:ts=4:sw=4
