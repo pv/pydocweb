4c6406f35d60ad41ec50e9ceb9f73990b4e51703
Emmanuelle Gouillart <gouillar@gimel>
2008-05-03 17:09:33 +0200

    *experimental* roundtrips work now!

--- numpydoc.py
+++ numpydoc.py
@@ -24,6 +24,7 @@ class MoinPage():
         mf = editmoin.MoinFile(filename, id)
         mf.read_raw(sourcefile)
         editmoin.sendfile(urlopener, self.url, mf)
+        editmoin.sendcancel(urlopener, self.url, mf)
 
     def retrieve_docstring(self):
         template = None
@@ -42,17 +43,34 @@ class MoinPage():
 
 class DocModificator():
 
-    def __init__(self, module, wiki_url = None, base_dir = None):
-        self.module = module
+    def __init__(self, wiki_url = None, base_module_dir = None):
+        """Creates doc modificator object from
+        - wiki_url: url address of the wiki where the doc is to be exposed, 
+        and possibly modified by users
+        - base_module_dir: directory where the module files are stored 
+        (could be a bzr branch e.g.)
+        """
+        if base_module_dir == None:
+            base_module_dir = 'numpy'
+        self.module = __import__(base_module_dir)
         self.wiki_url = wiki_url
-        self. base_dir = base_dir
+        self. base_dir = base_module_dir
 
     def get_docs(self):
+        """Returns a list of functions to be documented
+            
+        Uses utilities from module inspect.
+        """
         self.module_names = [name for name in inspect.getmembers(self.module) if \
         (callable(name[1]) and (inspect.getdoc(name[1]) is not None) \
         and inspect.isfunction(name[1]))]
 
     def write_doc_to_wiki(self):
+        """Writes wiki pages from the module's documentation
+
+        Writes a front page with a list of all function, linking to individual 
+        wiki pages with the documentation for each function. Uses the class MoinPage. 
+        """
         mfp = MoinPage(self.wiki_url)
         # Open temporary file for storing information. 
         # This is a hack to reuse code in editmoin 
@@ -77,42 +95,71 @@ class DocModificator():
             mf.write_file('temp') # write to page
 
     def write_doc_from_wiki(self):
-        fl = filelist(self.base_dir)
-        fl = [file for file in fl if file[-3:]=='.py']
-        for n,fn in self.module_names:
-            print fn
-            n_url = os.path.join(self.wiki_url, n)
-            mf = MoinPage(n_url)
-            str = mf.retrieve_docstring()
-            self.write_fndoc_from_string(fn, str, fl)
+        """Writes doc from a wiki to files in a directory, replacing the old 
+        docstrings by the new docstrings from the wiki
+            
+        Loops over functions in the module, retrieves doc from the corresponding page
+        in the wiki, and calls the function self.write_fndoc_from_string to search for 
+        the file where to replace the docstring, and replace the docstring. 
+        """
+        #fl = filelist(self.base_dir)
+        # Keeps only .py extensions
+        #fl = [file for file in fl if file[-3:]=='.py']
+        for n,fn in self.module_names: 
+            if n=='size':
+                print fn
+                n_url = os.path.join(self.wiki_url, n) #url of the wiki page
+                mf = MoinPage(n_url)
+                #try:
+                str = mf.retrieve_docstring()
+                self.write_fndoc_from_string(fn, str)
+            #except:
+            #    print "wiki page does not exist yet!"
 
-
-    def write_fndoc_from_string(self,fn,str,fl):
+    def write_fndoc_from_string(self,fn,str):
+        """
+            
+        """
+        # List of files where we search for the doc string
+        #fl = filelist(self.base_dir)
+        # Keeps only .py extensions
+        #fl = [file for file in fl if file[-3:]=='.py']
         fndoc = fn.__doc__
         indent_lv = indent_level(fndoc)
-        str = string.join(str.split('\n'),'\n'+string.ljust('',indent_lv))
+        # Transforms the string to have the correct indentation level
         sourcefile = inspect.getsourcefile(fn)
-        file_list = [fi for fi in fl if os.path.basename(sourcefile)==os.path.basename(fi)]
-        for file in file_list:
-            fid = open(file)
-            str_file = fid.read()
+        # shortlist files in the directory that may contain the docstring 
+        # to be replaced
+        #file_list = [fi for fi in fl if os.path.basename(sourcefile)==os.path.basename(fi)]
+        #for file in file_list:
+        fid = open(sourcefile)
+        str_file = fid.read()
+        fid.close()
+        if fndoc in str_file:
+            str = string.join(str.split('\n'),'\n'+string.ljust('',indent_lv))
+            print "got it!", sourcefile
+            fid = open(sourcefile, 'w')
+            new_st = string.replace(str_file, fndoc, str)
+            fid.write(new_st)
             fid.close()
-            if fndoc in str_file:
-                print "trouve", file
-                fid = open(file, 'w')
-                new_st = string.replace(str_file,fn.__doc__,str)
-                fid.write(new_st)
-                fid.close()
         
 
-
 def function_doc(n,fn):
+    """For a function fn_name in module mod, returns a doc string correctly 
+    formatted in rst with:
+    - the name of the function 
+    - the signature of the function (arguments)
+    - the docstring
+    
+    This string is meant to be directly fed to a wiki page.
+    """
     n_doc = inspect.getdoc(fn) # doc
     argspec = inspect.getargspec(fn) # arguments
     args = inspect.formatargspec(*argspec)
     doc_str = "== %s ==\n"%n + "{{{\n#!rst\n"+'**%s** '%(n)+args+'\n\n'+ \
         inspect.getdoc(fn) + "\n}}}\n"
     return doc_str
+
 
 def filelist(base_dir):
     fl = []
@@ -122,7 +169,7 @@ def filelist(base_dir):
 
 def indent_level(docstr):
     if not("\n" in docstr):
-        return 0
+        return 4
     else:
         st = docstr.split('\n')[-1]
         if string.find(st, st.lstrip()):
