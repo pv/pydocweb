29e2fe9575484ccdca2f500af528f4aa175e05be
Pauli Virtanen <ptvirtan@sd-2116>
2008-05-10 00:01:34 +0200

    Stylistic changes to docstrings

--- pydoc-moin.py
+++ pydoc-moin.py
@@ -376,7 +376,7 @@ def cmd_moin_upload_local(args):
 
         # Upload a sample discussion page
         _moin_upload_underlay_page(underlay_dir, page_name + "/Discussion",
-            "= Discussion =\n\n[[Action(edit)]]\n")
+            "=== Discussion ===\n\n[[Action(edit)]]\n")
 
     # XXX: leave deleting pages for the site maintainers to do manually.
     #      It's too unsafe to do here.
@@ -847,7 +847,7 @@ class MoinFormatter(object):
         t = ""
         t += self.title(el, titlechar)
         if 'type' in el.attrib:
-            t += "Type: {{{%s}}}\n" % self.partlink(el.attrib['type'])
+            t += "Type: %s\n" % self.partlink(el.attrib['type'])
         if el.attrib.get('is-repr'):
             t += "{{{\n%s\n}}}" % el.text.decode("string-escape")
         else:
@@ -859,8 +859,7 @@ class MoinFormatter(object):
     def format(self, el):
         t = ("## NOTE: This page is automatically generated.\n"
              "##       Only edit portions between BEGIN DOCSTRING and END DOCSTRING.\n"
-             "##       To add additional information, you can also edit the page\n"
-             "##       %s/Extra Documentation\n"
+             "##       To discuss this documentation, edit the discussion page\n"
              ) % self.target(el.attrib['id'])
         if el.tag == "module":
             t += self.fmt_module(el)
