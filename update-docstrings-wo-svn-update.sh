187ad93ca672742579f1d9ca88e6018f84dd5eaf
Pauli Virtanen <pauli.virtanen@iki.fi>
2008-08-12 21:09:45 +0300

    Script to update docstrings without running 'svn update' first

---
+++ update-docstrings-wo-svn-update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+umask 0002
+PYTHONPATH="$PWD/..:$PYTHONPATH" DJANGO_SETTINGS_MODULE="pydocweb.settings" python -c 'import pydocweb.docweb.models; pydocweb.docweb.models.update_docstrings(False)'
