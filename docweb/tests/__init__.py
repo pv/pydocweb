cbc7c5f4c83d9941f5477076f151631aa5057e4c
Pauli Virtanen <pauli.virtanen@iki.fi>
2008-10-14 02:47:24 +0300

    Add very simplistic functional tests

---
+++ docweb/tests/__init__.py
@@ -0,0 +1,39 @@
+import unittest
+from django.test import TestCase
+
+
+class AccessTests(TestCase):
+    """
+    Simple tests that check that basic pages can be accessed
+    and they contain something sensible.
+    """
+    
+    def test_docstring_index(self):
+        response = self.client.get('/docs/')
+        self.failUnless('All docstrings' in str(response))
+
+    def test_wiki_new_page(self):
+        response = self.client.get('/Some New Page Or Not/')
+        self.failUnless('Create new' in str(response))
+
+    def test_changes(self):
+        response = self.client.get('/changes/')
+        self.failUnless('Recent changes' in str(response))
+
+    def test_search(self):
+        response = self.client.get('/search/')
+        self.failUnless('Fulltext' in str(response))
+
+    def test_stats(self):
+        response = self.client.get('/stats/')
+        self.failUnless('Overview' in str(response))
+
+    def test_patch(self):
+        response = self.client.get('/patch/')
+        self.failUnless('Generate patch' in str(response))
+
+    def test_non_authenticated(self):
+        for url in ['/merge/', '/control/', '/accounts/password/']:
+            response = self.client.get(url)
+            self.failUnless(('Location: http://testserver/accounts/login/?next=%s'%url)
+                            in str(response))
