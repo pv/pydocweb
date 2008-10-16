081b6a35eb29a4828976d5ac30d03d31383734c7
Pauli Virtanen <pauli.virtanen@iki.fi>
2008-10-17 00:59:06 +0300

    Hack in (a bit ugly) navigation tabs to other sites

--- docweb/context_processors.py
+++ docweb/context_processors.py
@@ -2,7 +2,9 @@ import pydocweb.docweb.models as models
 
 def media_url(request):
     site = models.Site.objects.get_current()
+    sites = models.Site.objects.all()
     from django.conf import settings
     return {'MEDIA_URL': settings.MEDIA_URL,
             'SITE_PREFIX': settings.SITE_PREFIX,
-            'EDITOR_NAME': site.name}
+            'OTHER_SITES': [s for s in sites if s != site],
+            'CURRENT_SITE': site}
