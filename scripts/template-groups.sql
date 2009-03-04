c09ef1bbfd3823163099f246f980f921ba8e41d3
Pauli Virtanen <pauli.virtanen@iki.fi>
2009-03-04 02:14:36 +0200

    Fix SQLiteism in scripts/template-groups.sql

--- scripts/template-groups.sql
+++ scripts/template-groups.sql
@@ -1,12 +1,12 @@
 BEGIN TRANSACTION;
 
-INSERT INTO "auth_group" VALUES(1, 'Editor');
-INSERT INTO "auth_group_permissions" (group_id, permission_id)
+INSERT INTO auth_group VALUES(1, 'Editor');
+INSERT INTO auth_group_permissions (group_id, permission_id)
 SELECT 1, id FROM auth_permission WHERE codename IN
 ('change_wikipage', 'change_docstring', 'change_reviewcomment');
 
-INSERT INTO "auth_group" VALUES(2, 'Reviewer');
-INSERT INTO "auth_group_permissions" (group_id, permission_id)
+INSERT INTO auth_group VALUES(2, 'Reviewer');
+INSERT INTO auth_group_permissions (group_id, permission_id)
 SELECT 2, id FROM auth_permission WHERE codename IN
 ('change_wikipage', 'change_docstring', 'change_reviewcomment',
  'can_review');
