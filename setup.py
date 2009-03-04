e7a573102e8c94eb2d76c8700f3246427f58334a
Pauli Virtanen <pauli.virtanen@iki.fi>
2009-03-04 02:23:33 +0200

    Add setup.py and bump version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "pydocweb",
+    version = "0.4",
+    packages = find_packages(),
+    author = "Pauli Virtanen",
+    author_email = "pav@iki.fi",
+    description = "Collaborative Python docstring editor on the web",
+    url = "http://code.google.com/p/pydocweb/",
+    include_package_data = True
+)
